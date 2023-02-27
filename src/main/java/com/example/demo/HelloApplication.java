a151b5494798149b0347342995976d51e4f6fec8
Phat Panhareach <panhareach.phat@student.cadt.edu.kh>
2023-02-27 22:41:49 +0700

    makes: design register page

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +11,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("sample.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("sign-up.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setFullScreen(true);
+        Image icon = new Image("https://assets.stickpng.com/images/61f7cd1767553f0004c53e6e.png");
+        stage.getIcons().add(icon);
         stage.setTitle("Library Management System");
         stage.setScene(scene);
         stage.show();
