d7125116aa9beb4a0fbb10b8959ef240c85a9082
Phat Panhareach <111555521+Re4ch-Jay@users.noreply.github.com>
2023-03-06 15:15:50 +0700

    added: fixes

---
+++ src/bookshopmanagementsystem/database.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bookshopmanagementsystem;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class database {
+    
+    public static Connection connectDb(){
+        
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/book", "root", "root"); // address, database username, database password
+            return connect;
+        }catch(Exception e){e.printStackTrace();}
+        return null;
+    }
+    
+}
